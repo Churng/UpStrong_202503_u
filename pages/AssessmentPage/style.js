d32fc175b00b84afa7f42672fb3bb822fae334d9
Lynne.Lee <61216031+Churng@users.noreply.github.com>
2025-06-27 13:07:19 +0800

    更改為大寫看看

--- pages/AssessmentPage/style.js
+++ pages/AssessmentPage/style.js
@@ -392,7 +392,7 @@ $(document).ready(function () {
 		// 確保 `params` 存在
 		//github更改路徑
 		if (params && params.workOrderID) {
-			window.location.href = `/UpStrong_202503_u/pages/AssessmentPage/question/index.html?workOrderID=${params.workOrderID}`;
+			window.location.href = `/UpStrong_202503_u/pages/AssessmentPage/question/Index.html?workOrderID=${params.workOrderID}`;
 		} else {
 			console.warn("缺少 workOrderID，無法跳轉");
 		}
